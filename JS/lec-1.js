c6ee1d6684c1beeb2cae663a87edd4e04b2a33a8
Drona-Srivastava <srivastavadrona@gmail.com>
2024-05-20 22:39:54 +0530

    Lec-1

---
+++ JS/lec-1.js
@@ -0,0 +1,15 @@
+//Variables
+// JS is dynamic typing language -> a variable type can be changed in runtime 
+// other is static typing where variable type can not be changed like in C/C++ once declared an int it can not be changed to float
+//var is a container in which all types are stored no need to specify which type it is
+let a = 67 // var in place of let would work the same
+console.log(a)
+a = "Drona"
+console.log(a) 
+//hence we do not need to specify the type and can be changed in runtime
+const b = 90
+console.log(b)
+//b = 34 can not be changes since we used cont (constant)
+//console.log(b)
+
+// let keyword lets us create local variable with same name whereas var does not allow us to use same name for another local variable if we try to do so the global variable value will be changes
